ff9e67c25add90bf9934802c3e04df1d96bc5755
rosec <906364930@qq.com>
2023-12-10 05:46:38 +0000

    attn可视化粗糙版

--- src/vis/attn_vis.py
+++ src/vis/attn_vis.py
@@ -12,46 +12,62 @@ from torch import nn
 from collections.abc import Iterable  # for Python 3.3 and above
 from typing import Callable, Dict
 from torchvision import transforms
+import matplotlib.font_manager as font_manager
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import torch.nn.functional as F
 
+# 添加字体路径
+font_path = '/usr/share/fonts/truetype/msttcorefonts/Times_New_Roman.ttf'
+font_prop = font_manager.FontProperties(fname=font_path, size=12)  # 可以指定字体大小
 
-def visualize_attention_maps(attention_scores,j):
-    attention_scores = attention_scores.detach().numpy()  # 转换为NumPy数组
-    num_heads = attention_scores.shape[1]
-    fig, axs = plt.subplots(4, 3, figsize=(12, 16))
-    fig.suptitle('Attention Maps')
-
-    for i in range(num_heads):
-        row = i // 3
-        col = i % 3
-        attn_map = attention_scores[0, i]
-        attn_map = torch.from_numpy(attn_map)
-        # cos = torch.matmul(attn_map,attn_map.transpose(-1, -2))
-        # cos_mean = torch.mean(cos)
-        # cos = cos / torch.max(cos)*255
-        ce_all = 0
-        for m in range(1370):
-            for n in range(1370):
-                ce = F.cross_entropy(attn_map[m], attn_map[n])
-                ce_all = ce_all + ce
-        ce_all = ce_all / (1370^2)
+def visualize_attention_weights(extractor, image, save_path='attention_map.png'):
+    # 将图像输入模型以收集注意力权重
+    with torch.no_grad():
+        attention_maps = extractor(image)
+    
+    # 创建一个大图，以便存储所有的热力图
+    num_layers = len(next(iter(attention_maps.values())))
+    num_heads = attention_maps[next(iter(attention_maps))][0]['output'].size(0)
+    fig, axes = plt.subplots(nrows=3, ncols=num_layers, figsize=(num_layers * 3, 3 * 3), dpi=100)
+    
+    # 定义每行代表的矩阵类型
+    row_labels = ['attn_scores', 'group_weight', 'attn_weights']
+    
+    # 遍历每种权重和每层的权重
+    for row, (key, feature_list) in enumerate(attention_maps.items()):
+        # 在最左侧添加矩阵类型的注释，使用指定的字体
+        axes[row, 0].text(-0.5, 0.5, row_labels[row], va='center', ha='right',
+                           fontsize=12, fontproperties=font_prop, transform=axes[row, 0].transAxes)
         
-        # CE = F.cross_entropy(attn_map, attn_map)
-        
-        
-        axs[row, col].imshow(attn_map, cmap='hot', interpolation='nearest')
-        
-        rank = 1
-        # axs[row, col].set_title(f'Head {i+1} (cos: {cos:.2f})')
-        axs[row, col].set_title(f'Head {i+1} ce {ce_all:2f}')
-        axs[row, col].axis('off')
-
-    plt.tight_layout()
-    plt.savefig(f'/root/workspace/TransFG/attn_maps/{j}.png')  # 保存图像到当前文件夹下的attention_maps.png
-    plt.close()  # 关闭绘图窗口
+        for col, feature in enumerate(feature_list):
+            # 归一化注意力权重
+            attn_matrix = feature['output'].squeeze(0)  # 假设 batch size 是 1
+            attn_matrix = attn_matrix / attn_matrix.sum(dim=-1, keepdim=True)
+            attn_matrix = attn_matrix.to(dtype=torch.float32).cpu().numpy()
+            
+            # 绘制热力图
+            ax = axes[row, col]
+            im = ax.imshow(attn_matrix, cmap='coolwarm', aspect='auto')
+            
+            # 仅在顶部的子图上标注层数，使用指定的字体
+            if row == 0:
+                ax.set_title(f'Layer {col+1}', fontsize=14, fontproperties=font_prop, color='red')
+            
+            # 关闭坐标轴提升清晰度
+            ax.axis('off')
+    
+    # 调整布局和添加颜色条
+    fig.subplots_adjust(right=0.85, hspace=0.2, wspace=0.2)
+    cbar_ax = fig.add_axes([0.87, 0.15, 0.03, 0.7])
+    cbar = fig.colorbar(im, cax=cbar_ax)
+    cbar.ax.tick_params(labelsize=10)  # 设置颜色条刻度字体大小
+    cbar.ax.yaxis.set_ticks_position('left')  # 将颜色条刻度放置到左侧
+    
+    # 保存可视化结果
+    plt.savefig(save_path, format='png', bbox_inches='tight')
+    plt.close(fig)
 
 def print_module_attr_names(module, prefix=''):
     for name in module.__dict__:
@@ -116,26 +132,6 @@ if __name__ == '__main__':
 
     # Forward pass through the extractor, which will collect the intermediate features
     intermediate_features = extractor(img)
-    a = 1
 
-    # for name, param in model.named_parameters():
-    #     if 'attn_scores' in name:
-    #         print(name, param)
-    # print_module_attr_names(model.net)
+    visualize_attention_weights(extractor, image=img)
 
-
-    
-    # for i in range(11):
-    #     attn_scores_str = [f"transformer.encoder.layer.{i}.attn.softmax"]
-    #     attn_scores = FeatureExtractor(model, layers=last_layer)
-        
-    #     input = Image.open("/root/SharedData/datasets/CUB_200_2011/data_splited/val/001.Black_footed_Albatross/Black_Footed_Albatross_0001_796111.jpg")
-    #     # input = Image.new('RGB', (448, 448), (255, 255, 255))
-    #     input = test_transform(input)
-    #     input = input.unsqueeze(0)  # 添加batch维度
-    #     # input = torch.randn(3,3,448,448)
-    #     features = vit_features(input)
-    #     # print({name: output.shape for name, output in features.items()})
-    #     item = features[last_layer[0]]
-
-    #     visualize_attention_maps(item,j)
